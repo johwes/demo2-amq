4867ae456937499be197ad9d394bf89521be3ccf
Burr Sutter <burrsutter@gmail.com>
2018-10-17 16:41:49 -0500

    hello

--- worker/src/main/java/io/openshift/booster/messaging/Worker.java
+++ worker/src/main/java/io/openshift/booster/messaging/Worker.java
@@ -129,7 +129,7 @@ public class Worker extends AbstractVerticle {
     if (reverse) {
       text = new StringBuilder(text).reverse().toString();
     }
-    return text + " X";
+    return "Hello " + text;
     // return text;
   }
 
