072b46c2c7e24573094a38ce539e3408187af210
Burr Sutter <burrsutter@gmail.com>
2018-10-20 15:20:25 -0400

    adding env vars script

---
+++ scripts/exportEnvVars.sh
@@ -0,0 +1,8 @@
+export ONPREM_SUFFIX=192.168.99.101.nip.io
+export AWS_SUFFIX=13.56.49.163.nip.io
+export GCP_SUFFIX=35.230.115.194.nip.io
+export AZURE_SUFFIX=40.78.82.99.nip.io
+echo $ONPREM_SUFFIX
+echo $AWS_SUFFIX
+echo $GCP_SUFFIX
+echo $AZURE_SUFFIX
