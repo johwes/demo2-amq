c14a38010bdf7ac1709c3ccc1c8680d0b1dd705e
Burr Sutter <burrsutter@gmail.com>
2018-11-03 20:54:01 -0400

    tweaked .gitignore, new IP

--- scripts/exportEnvVars.sh
+++ scripts/exportEnvVars.sh
@@ -1,4 +1,4 @@
-export ONPREM_SUFFIX=192.168.99.101.nip.io
+export ONPREM_SUFFIX=192.168.99.100.nip.io
 export AWS_SUFFIX=13.56.49.163.nip.io
 export GCP_SUFFIX=35.230.115.194.nip.io
 export AZURE_SUFFIX=40.78.82.99.nip.io
