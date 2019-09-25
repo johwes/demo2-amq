cc18b36e901415234419622ec342020d9fdc2e20
Burr Sutter <burrsutter@gmail.com>
2019-09-25 11:37:51 -0400

    making GCP on-prem

--- scripts/exportEnvVars.sh
+++ scripts/exportEnvVars.sh
@@ -1,8 +1,8 @@
-export ONPREM_SUFFIX=apps-crc.testing
+export ONPREM_SUFFIX=apps.gcp.burrsutter.dev
 export AWS_SUFFIX=apps.aws.burrsutter.org
-export GCP_SUFFIX=apps.gcp.burrsutter.dev
+#export GCP_SUFFIX=apps.gcp.burrsutter.dev
 export AZURE_SUFFIX=apps.azr.burrsutter.net
 echo $ONPREM_SUFFIX
 echo $AWS_SUFFIX
-echo $GCP_SUFFIX
+# echo $GCP_SUFFIX
 echo $AZURE_SUFFIX
