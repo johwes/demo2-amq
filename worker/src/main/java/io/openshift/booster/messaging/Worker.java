4b5e9dbb8037102de0881f629bc09f55861665cf
Burr Sutter <burrsutter@gmail.com>
2019-11-08 15:03:15 -0500

    merhaba

--- worker/src/main/java/io/openshift/booster/messaging/Worker.java
+++ worker/src/main/java/io/openshift/booster/messaging/Worker.java
@@ -129,7 +129,7 @@ public class Worker extends AbstractVerticle {
     if (reverse) {
       text = new StringBuilder(text).reverse().toString();
     }
-    return "Aloha " + text;
+    return "Merhaba " + text;
     // return text;
   }
 
